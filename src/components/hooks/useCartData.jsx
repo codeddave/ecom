89b714c640f3a7d194286684a6f63815d62348d2
Dvaid Adeleye <drizzzydave@gmail.com>
2021-06-08 01:03:24 +0100

    updates

--- src/components/hooks/useCartData.jsx
+++ src/components/hooks/useCartData.jsx
@@ -4,13 +4,13 @@ export function useCartData() {
   const [cartData, setCartData] = useState(
     JSON.parse(localStorage.getItem("cartData"))
   );
-  const cartLength = JSON.parse(localStorage.getItem("cartData")).cart.length;
+  const cartLength = JSON.parse(localStorage.getItem("cartData"))?.cart.length;
   useEffect(() => {
     setCartData(JSON.parse(localStorage.getItem("cartData")));
   }, [cartLength]);
 
   return {
-    cartData,
+    cartData: cartData ? cartData : null,
     setCartData,
   };
 }
